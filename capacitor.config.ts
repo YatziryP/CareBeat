c20344ef4cf65bfb0a627cf364c3c8306d4da722
yatziry <a3520110146@alumno.uttehuacan.edu.mx>
2023-06-05 21:59:38 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CareBeat',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
